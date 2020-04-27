c8be2e55659502a19d460426aeb0f5a8ac94ec9a
KsenyaBantsevich <44980734+KsenyaBantsevich@users.noreply.github.com>
2020-04-27 18:56:42 +0300

    Add files

---
+++ lab1.cpp
@@ -0,0 +1,10 @@
+﻿
+
+#include "Conveyer.h"
+
+int main() {
+    vector<int> one{ 34,54, 47, 63,51,45,47,55,51 };
+    vector<int> two{ 51,39, 35, 50 ,34,50,45,54,63};
+    auto Dx = new Conveyer;
+    Dx->calculate(one, two);
+}
